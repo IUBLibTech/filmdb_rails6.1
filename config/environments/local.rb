48ebbc20ac5d749acf7b9cabecc90dff454bc0ab
jaalbrec <jaalbrec@indiana.edu>
2022-08-23 10:51:38 -0400

    Updated development credentials to point at correct database

--- config/environments/local.rb
+++ config/environments/local.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  #config.web_console.development_only = false
+  config.web_console.development_only = false
 end
