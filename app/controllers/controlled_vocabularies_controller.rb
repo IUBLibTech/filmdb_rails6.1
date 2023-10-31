c99c1b143d44a9af82701808115dbf63356ce108
jaalbrec <jaalbrec@indiana.edu>
2023-10-31 10:21:31 -0400

    Fixed a bug where distinct was being called after a pluck - should have been uniq

--- app/controllers/controlled_vocabularies_controller.rb
+++ app/controllers/controlled_vocabularies_controller.rb
@@ -9,7 +9,7 @@ class ControlledVocabulariesController < ApplicationController
 
   def create
     authorize ControlledVocabulary
-    model_type = ControlledVocabulary.where(model_attribute: params[:controlled_vocabulary][:model_attribute]).pluck(:model_type).distinct.first
+    model_type = ControlledVocabulary.where(model_attribute: params[:controlled_vocabulary][:model_attribute]).pluck(:model_type).uniq.first
     cv = ControlledVocabulary.new(model_type: model_type, model_attribute: params[:controlled_vocabulary][:model_attribute], value: params[:controlled_vocabulary][:value], active_status: true)
     cv.save!
     flash[:notice] = "The new #{model_type == 'Title' ? ControlledVocabulary.human_attribute_name(cv.model_attribute) : model_type.gsub("Title", '')} term <i>#{cv.value}</i> was created.".html_safe
@@ -22,8 +22,8 @@ class ControlledVocabulariesController < ApplicationController
   private
   def get_map
     @map = {}
-    ControlledVocabulary.where("model_type like 'Title%'").pluck(:model_type).distinct.each do |mt|
-      ControlledVocabulary.where(model_type: mt).pluck(:model_attribute).distinct.each do |ma|
+    ControlledVocabulary.where("model_type like 'Title%'").pluck(:model_type).uniq.each do |mt|
+      ControlledVocabulary.where(model_type: mt).pluck(:model_attribute).uniq.each do |ma|
         @map[ma] = ControlledVocabulary.where(model_type: mt, model_attribute: ma).order(:value).pluck(:value)
       end
     end
