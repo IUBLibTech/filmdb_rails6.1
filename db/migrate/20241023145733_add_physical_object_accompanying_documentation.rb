f7d09e253c6b3f00b161a0db904477aa51cf9c50
Andrew Albrecht <jaalbrec@indiana.edu>
2024-11-01 09:45:05 -0400

    Same issue as previous - class name collision with the migration file and model file for PhysicalObjectAcccompanyingDocumentation

--- db/migrate/20241023145733_add_physical_object_accompanying_documentation.rb
+++ db/migrate/20241023145733_add_physical_object_accompanying_documentation.rb
@@ -1,4 +1,4 @@
-class PhysicalObjectAccompanyingDocumentation < ActiveRecord::Migration[6.1]
+class AddPhysicalObjectAccompanyingDocumentation < ActiveRecord::Migration[6.1]
   def change
     create_table :physical_object_accompanying_documentations do |t|
       t.integer :physical_object_id
