908804b37e4559fe0fdd9d8f40fe290990b8b2f9
Andrew Albrecht <jaalbrec@indiana.edu>
2025-02-03 10:29:27 -0500

    Rails 7.0 workink

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,5 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
 # Use tag or commit hash as version string; sub removes tag from tag-hash output
 APP_VERSION = `git describe --always`.sub(/.*-g/, '') unless defined? APP_VERSION
