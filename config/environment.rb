68f8d5425a8dc06bf84326c5dc23cb1b89b28f24
Andrew Albrecht <jaalbrec@indiana.edu>
2025-02-03 11:16:06 -0500

    Filmdb working under Rails 7.1

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
 # Use tag or commit hash as version string; sub removes tag from tag-hash output
 APP_VERSION = `git describe --always`.sub(/.*-g/, '') unless defined? APP_VERSION
